1fe999c48bf4d04345ddad8278c4a1bfd04602c1
MSI <MSI@GOTOSLEEP249>
2023-06-24 15:33:36 +0700

    Add project files.

---
+++ WebBanHangOnline/Models/EF/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tbOrderDetail")]
+    public class OrderDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
